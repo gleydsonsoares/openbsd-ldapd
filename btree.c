329df7e55d59817783cdcaeaca3dddbc19be29cd
martinh <martinh>
2010-06-13 06:55:33 +0000

    Don't expose typedefs of pgno_t and indx_t in the public header file, move them to btree.c. Also don't use exact-width integer types in public btree api.

--- btree.c
+++ btree.c
@@ -56,6 +56,9 @@
 
 #define F_ISSET(w, f)	 (((w) & (f)) == (f))
 
+typedef uint32_t	 pgno_t;
+typedef uint16_t	 indx_t;
+
 /* There are four page types: meta, index, leaf and overflow.
  * They all share the same page header.
  */
@@ -957,7 +960,7 @@ fail:
 }
 
 struct btree *
-btree_open_fd(int fd, uint32_t flags)
+btree_open_fd(int fd, unsigned int flags)
 {
 	int		 fl, rc;
 	struct btree	*bt;
@@ -1021,7 +1024,7 @@ fail:
 }
 
 struct btree *
-btree_open(const char *path, uint32_t flags, mode_t mode)
+btree_open(const char *path, unsigned int flags, mode_t mode)
 {
 	int		 fd, oflags;
 	struct btree	*bt;
