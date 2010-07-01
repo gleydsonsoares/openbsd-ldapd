53d275fd4576310b5d57d2b640352a28c40cb6e6
martinh <martinh>
2010-07-01 04:21:41 +0000

    If the length of an element being read is larger than what is available in the buffer, return immediately. This fixes reading large messages, and allows bad requests to be cancelled earlier.
    Originally from Alexander Schrijver, tweaked by me.

--- ber.c
+++ ber.c
@@ -1065,6 +1065,13 @@ ber_read_element(struct ber *ber, struct ber_element *elm)
 	DPRINTF("ber read element size %zd\n", len);
 	totlen += r + len;
 
+	/* If using an external buffer and the total size of the element
+	 * is larger then the external buffer don't bother to continue. */
+	if (ber->fd == -1 && totlen > ber->br_rend - ber->br_rbuf) {
+		errno = ECANCELED;
+		return -1;
+	}
+
 	elm->be_type = type;
 	elm->be_len = len;
 	elm->be_class = class;
